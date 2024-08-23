127350146ba5370532acf6f95198f461208c5120
Srivatsa V <138861827+vatsa01@users.noreply.github.com>
2024-08-24 00:44:04 +0530

    Updated

--- assignment.sql
+++ assignment.sql
@@ -17,3 +17,20 @@ from (select s.pay_date, e.department_id, avg(s.amount) over(partition by s.pay_
       as month_avg, avg(s.amount) over (partition by s.pay_date, e.department_id)
       as dept_avg from Salary s inner join Employee e on s.employee_id = e.employee_id)
 as cte
+
+Problem 1 : Report Contiguos Dates
+
+with cte as (
+    select fail_date as date, 'failed' as period_state, rank() over(order by fail_date) as rnk
+    from failed where year(fail_date) = '2019'
+    union 
+    select success_date as date, 'succeeded' as period_state, rank() 
+    over(order by success_date) as rnk
+    from Succeeded where year(success_date) = 2019
+)
+
+select period_state, min(date) as start_date, max(date) as end_date 
+from (select *, (rank() over(order by date) - rnk) as diff from cte)
+as temp
+group by period_state, diff
+order by start_date
